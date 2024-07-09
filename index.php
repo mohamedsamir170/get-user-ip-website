cfff7e2271bfd3a022af550f600f8fb069ebb301
Mohamed Samir <mohamedsamir170569@gmail.com>
2024-07-09 16:50:58 +0300

    docker compose connected to sql

--- index.php
+++ index.php
@@ -1,32 +1,25 @@
 
 <?php
 
-    $db_server = 'db'; 
-    $db_user = 'mohamed';
-    $db_pass = 'Mohamed@1234';
-    $db_name =  'demo_website_db';
-    $conn = "";
-
-    try{
-        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-    }
-    catch(mysqli_sql_exception){
-        echo "failed to connect <br>";
-    }
-
+    $connect = mysqli_connect(
+        'db', # service name
+        'mohamed', # username
+        'Mohamed@1234', # password
+        'demo_website_db' # db table
+    );
     echo "Hello World! <br>";
 
     $ip = $_SERVER['REMOTE_ADDR'];
     $time = date('Y-m-d H:i:s');
 
     $query = "SELECT * FROM users WHERE ip = '$ip'";
-    $result = mysqli_query($conn,$query);
+    $result = mysqli_query($connect,$query);
     if(mysqli_fetch_assoc($result) == 0)
     {
         $query2 = "INSERT INTO users (ip, date) VALUES ('$ip', '$time')";
-        mysqli_query($conn,$query2);
+        mysqli_query($connect,$query2);
         // echo "User Added <br>";
     }
     echo "Hello! Your IP address is $ip and the current time is $time.<br>";
-    mysqli_close($conn);
+    mysqli_close($connect);
 ?>
